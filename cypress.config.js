966fe0bc5ec03625d36919a88211fa7c76cf4b5a
Ayo <ayobami@gehilaw.com>
2024-06-30 17:45:49 +0100

    + bdd setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('file:preprocessor', createBundler());
+      // Implement node event listeners here
     },
+    specPattern: ["cypress/e2e/features/*.feature", "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"],
+    chromeWebSecurity: false,
+    baseUrl: "https://demo.seleniumeasy.com/"
   },
 });
